b40a4c593ea8ac9bdf618542bfb17ef1f2b9755e
BitLorax <willjhliang@gmail.com>
2020-06-02 18:54:38 -0700

    Altered dataset
    Former-commit-id: 47bd112daf3c2780856199571bfe72f95cb70914 [formerly a04169eadbf18653ff18816c9935ed4a645a8bc6]
    Former-commit-id: ea430f7ddee26461efcae1a5c0eab76c241cfd45

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,16 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+
+def display(displayList):
+    plt.figure(figsize=(15, 15))
+    for i in range(len(displayList)):
+        plt.subplot(1, len(displayList), i + 1)
+        plt.imshow(displayList[i])
+        plt.axis('off')
+    plt.show()
+
+
 with h5py.File('nyuDepth_v2.mat', 'r') as f:
     imgs = f.get('images')[()]
     depths = f.get('depths')[()]
@@ -12,11 +22,6 @@ with h5py.File('nyuDepth_v2.mat', 'r') as f:
 m = imgs.shape[0]
 imgs = np.moveaxis(imgs, 1, -1)
 depths = np.expand_dims(depths, axis=-1)
-
-random = np.arange(m)
-np.random.shuffle(random)
-imgs = imgs[random]
-depths = depths[random]
 
 
 def resize(data, scale):
@@ -29,8 +34,8 @@ def resize(data, scale):
     return ret
 
 
-imgs = resize(imgs, .6)
-depths = resize(depths, .6)
+imgs = resize(imgs, .4)
+depths = resize(depths, .4)
 
 
 def getSubpics(imgData, depthData, subWidth, subHeight, xStride, yStride):
@@ -53,14 +58,23 @@ def getSubpics(imgData, depthData, subWidth, subHeight, xStride, yStride):
     return imgRet, depthRet
 
 
-imgs, depths = getSubpics(imgs, depths, 128, 128, 128, 80)
+imgs, depths = getSubpics(imgs, depths, 128, 128, 128, 64)
+
+flipImgs = [cv2.flip(img, 1) for img in imgs]
+flipDepths = [cv2.flip(depth, 1) for depth in depths]
+imgs = np.concatenate((imgs, np.array(flipImgs)))
+depths = np.concatenate((depths, np.array(flipDepths)))
 
 m = imgs.shape[0]
+random = np.arange(m)
+np.random.shuffle(random)
+imgs = imgs[random]
+depths = depths[random]
+
 print(imgs.shape)
 print(depths.shape)
 
 BATCH_SIZE = 64
-
 for i in range(math.ceil(1.0 * m / BATCH_SIZE)):
     s = BATCH_SIZE * i
     e = min(BATCH_SIZE * (i + 1), m)
