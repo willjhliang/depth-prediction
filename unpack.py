009f32887d582e485da38d96761f97e89230bd42
BitLorax <willjhliang@gmail.com>
2020-06-09 20:54:14 -0700

    Test commit
    Former-commit-id: c204e2fa979f6d229bc3b22ea47c1786b6f6a1b6 [formerly 03fd035a4a0da8afe7d47b3039621666b4f0c602] [formerly f6a0b63c9623e370b8c8c8c8353aa5e22c828faf [formerly b1a4df8640d7333d0367ad3996f2561a15b5fc9c]]
    Former-commit-id: a12441b238402a1e79366324c97ebf3d9e172b78 [formerly 44f0dda3a8cf2c4f3ee1d31ecc2dc6f2caf6c209]
    Former-commit-id: 2f541544612034672ac439583fe530056b4a8849
    Former-commit-id: 9b4d984793d8c8a462cf009160eeedabd9b01c66

--- unpack.py
+++ unpack.py
@@ -1,7 +1,6 @@
 
 import numpy as np
 import h5py
-import cv2
 
 
 with h5py.File('nyu_depth_v2_labeled.mat', 'r') as f:
@@ -10,21 +9,11 @@ with h5py.File('nyu_depth_v2_labeled.mat', 'r') as f:
 
 m = imgs.shape[0]
 imgs = np.moveaxis(imgs, 1, -1)
-depths = np.expand_dims(depths, axis=-1)
 
-
-def resize(data, scale):
-    ret = []
-    for i in range(m):
-        img = data[i]
-        img = cv2.resize(data[i], (0, 0), fx=scale, fy=scale)
-        ret.append(img)
-    ret = np.array(ret)
-    return ret
-
-
-imgs = resize(imgs, .5)
-depths = resize(depths, .5)
+random = np.arange(m)
+np.random.shuffle(random)
+imgs = imgs[random]
+depths = depths[random]
 
 for i in range(m):
     np.save('nyuDepth/img' + str(i) + '.npy', imgs[i])
